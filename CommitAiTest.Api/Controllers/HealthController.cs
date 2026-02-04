d99b2497dc4ea8a1c7fedc86576ab3141c3681f5
Andrei Iliut <iliutandreicoman@gmail.com>
2026-02-04 15:53:03 +0200

    feat(api): Add health and weather forecast controllers
    - Added new HealthController with GET endpoint returning status "ok"
    - Updated WeatherForecastController to include "AIish" as a summary option
    - No changes to other code files

---
+++ CommitAiTest.Api/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommitAiTest.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { status = "ok" });
+    }
+}
